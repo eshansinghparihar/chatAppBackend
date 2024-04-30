4c98ffaae0f6d0dd2b9aa18f0b652792dcada3e4
SupreethaH <supreetha.h@apisero.com>
2024-04-30 13:22:07 +0530

    forgot password feat

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import createHttpError from "http-errors";
+import { UserModel } from "../models/index.js";
 import logger from "../configs/logger.config.js";
 import { searchUsers as searchUsersService } from "../services/user.service.js";
 export const searchUsers = async (req, res, next) => {
@@ -9,6 +10,24 @@ export const searchUsers = async (req, res, next) => {
       throw createHttpError.BadRequest("Oops...Something went wrong !");
     }
     const users = await searchUsersService(keyword, req.user.userId);
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+export const findUser = async (userId) => {
+  const user = await UserModel.findById(userId);
+  if (!user) throw createHttpError.BadRequest("Please fill all fields.");
+  return user;
+};
+export const doesUserExist = async (req, res, next) => {
+  try {
+    const keyword = req.query.email;
+    if (!keyword) {
+      logger.error("Please add an email query first");
+      throw createHttpError.BadRequest("Oops...Something went wrong!");
+    }
+    const users = await UserModel.find({ keyword })
     res.status(200).json(users);
   } catch (error) {
     next(error);
